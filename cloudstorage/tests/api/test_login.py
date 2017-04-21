3cd8ea8b1b55e77f7bc3c80160e706e8718c28b4
andrea <andrea.czar@gmail.com>
2017-04-20 21:50:43 -0700

    removed incorrect comments

--- cloudstorage/tests/api/test_login.py
+++ cloudstorage/tests/api/test_login.py
@@ -2,7 +2,6 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
-
 from cloudstorage.models import StorageUser
 
 
@@ -18,9 +17,7 @@ class LoginAPITests(APITestCase):
         self.user = user
 
     def test_login_success(self):
-        """
-        Ensure we can login
-        """
+
         url = '/api/login/'
         data = {'email': 'test@test.com', 'password': 'password'}
         response = self.client.post(url, data, format='json')
@@ -30,9 +27,7 @@ class LoginAPITests(APITestCase):
         self.assertEqual(response.data['token'], token.key)
 
     def test_login_cycles_token(self):
-        """
-        Ensure we can login
-        """
+
         url = '/api/login/'
         data = {'email': 'test@test.com', 'password': 'password'}
         response = self.client.post(url, data, format='json')
@@ -47,45 +42,35 @@ class LoginAPITests(APITestCase):
         self.assertNotEqual(response.data['token'], token.key)
 
     def test_wrong_password(self):
-        """
-        Ensure we can login
-        """
+
         url = '/api/login/'
         data = {'email': 'test@test.com', 'password': 'incorrect'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_wrong_email(self):
-        """
-        Ensure we can login
-        """
+
         url = '/api/login/'
         data = {'email': 'incorrect@test.com', 'password': 'password'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_missing_password(self):
-        """
-        Ensure we can login
-        """
+
         url = '/api/login/'
         data = {'email': 'test@test.com'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_missing_email(self):
-        """
-        Ensure we can login
-        """
+
         url = '/api/login/'
         data = {'password': 'password'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_missing_credentials(self):
-        """
-        Ensure we can login
-        """
+
         url = '/api/login/'
         data = {'boop': 'plumbus'}
         response = self.client.post(url, data, format='json')
