84c599e58f7ad89b4e90c07a8689534a3c165096
JNotelddim <jaredmiddleton3.14@gmail.com>
2017-04-29 14:04:02 -0700

    test file class

---
+++ cloudstorage/tests/unit/test_file.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from cloudstorage.models import File
+from django.core.files.uploadedfile import SimpleUploadedFile
+import mimetypes
+
+class TestFile(TestCase):
+
+    def test_mime_type(self):
+        test_file = File()
+        test_file.file = SimpleUploadedFile("file.jpg", b"file_content", content_type="image/jpeg")
+
+        test_file.set_mime_type()
+        self.assertEqual(test_file.mime_type, 'image/jpeg')
