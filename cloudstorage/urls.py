0cabf1c473853a2eeaeedfc3614b189db4b68bdd
Matthew Casiro <mattcasiro@gmail.com>
2017-04-29 17:54:53 -0700

    Add debug toolbar application

--- cloudstorage/urls.py
+++ cloudstorage/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
@@ -55,4 +56,7 @@ urlpatterns += [
 ]
 
 if settings.DEBUG:
+    urlpatterns = [
+                      url(r'^__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
