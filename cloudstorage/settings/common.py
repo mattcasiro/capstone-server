1f65636d89f85a2ae2b8a1a659812ae4d929d17f
JNotelddim <jaredmiddleton3.14@gmail.com>
2017-05-03 16:58:41 -0700

    Set rest framework defualt auth backends add token auth as backend

--- cloudstorage/settings/common.py
+++ cloudstorage/settings/common.py
@@ -142,7 +142,13 @@ MEDIA_URL = '/media/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     )
+
 }
 
 
